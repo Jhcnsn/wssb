9d4c4e845090ed3b4e7c7eb7c99b2c354aee027f
hp5 <huangpeiwu5@163.com>
2022-05-09 18:36:50 +0800

    hot-fix 删了Main2 Main1->HotMain

--- first/src/com/hpw/first/HotMain.java
+++ first/src/com/hpw/first/HotMain.java
@@ -4,7 +4,7 @@ package com.hpw.first;
  * @author HPW
  * @create 2022-05-09 18:08
  */
-public class Main1 {
+public class HotMain {
     public static void main(String[] args) {
         System.out.println("wocao");
         System.out.println("文明一点好吗？？？");
