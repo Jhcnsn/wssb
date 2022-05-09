582bcdfc45373a0a597eb0f5500face7215c7296
hp5 <huangpeiwu5@163.com>
2022-05-09 18:30:52 +0800

    文明一点可以吗？

---
+++ first/src/com/hpw/first/Main2.java
@@ -0,0 +1,11 @@
+package com.hpw.first;
+
+/**
+ * @author HPW
+ * @create 2022-05-09 18:28
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("wocao");
+    }
+}
